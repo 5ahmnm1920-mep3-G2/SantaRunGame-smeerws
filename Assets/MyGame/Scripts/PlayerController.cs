4507f1b2610022ec75077fdb5c56ecb23dc62999
smeerws <hello@smeerws.at>
2019-12-12 18:25:35 +0100

    add Controller for Santa, add Method Jump

---
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float jumpForce;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+    }
+}
