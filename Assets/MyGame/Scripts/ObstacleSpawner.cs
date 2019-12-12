dbb3d09ad73ec8e55b5123458f22b72297fccf33
smeerws <hello@smeerws.at>
2019-12-12 19:13:23 +0100

    initial spawn object script

---
+++ Assets/MyGame/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public bool gameOver = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        float waitTime = 0.5f;
+
+        while (!gameOver)
+        {
+            SpawnObstacle();
+
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        Instantiate(obstacle,transform.position,Quaternion.identity);
+    }
+}
